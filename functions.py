c8e7aef5523e3c72402c7330ab56d884f365e006
mhabib80 <57415632+mhabib80@users.noreply.github.com>
2022-06-20 08:46:07 -0700

    add tmp directory

--- functions.py
+++ functions.py
@@ -1,45 +1,5 @@
-import pandas as pd
-import numpy as np
+
 import matplotlib.pyplot as plt
-from matplotlib import ticker
-
-
-def sigmoid(x, k=3.627412246682911, b=-0.001164921989173435):
-  return 1 / (1 + np.exp(-k*(x-b)))
-
-
-def curve(p_name, contract_amount, start_date, completion_date,  curve, csrf=None):
-
-    date_range = pd.date_range(start_date, completion_date, freq='d')  # date range per day
-    dur = (date_range.max() - date_range.min()).days
-
-    if curve == 'trapezoidal':
-        sigmoid_input = np.linspace(-1.1, 1.1, dur + 1) #adjust the x values to better fit the S-Curve function
-        x = sigmoid(sigmoid_input)
-
-    else:
-        x = np.linspace(0, 1, dur + 1)
-
-    cum_earnings = (x * contract_amount).ravel()
-    data = pd.DataFrame(cum_earnings, index=date_range, columns=['cum_earnings'])
-    data.iloc[0] = 0
-    # data.iloc[-1] = contract_amount  # last period should be 100% contract amount
-    output = data.resample('M').cum_earnings.max().astype(int).to_frame()
-    output[p_name + '_Monthly'] = output['cum_earnings'].diff().fillna(output['cum_earnings']) #so 1st period won't be NaN
-    output.drop('cum_earnings', inplace=True, axis=1)
-    return output
-
-
-def plot_chart():
-    df = pd.read_csv('tmp/df.csv', index_col=[0])
-    ax = df.iloc[:, :-2].plot(kind='bar', stacked=True, figsize=(20, 15),
-                              color=['#06283D', '#1363DF', '#47B5FF', '#FF6D02', '#7577CD'])
-    ax2 = df['Cum_Monthly'].plot(c='red', ax=ax, secondary_y=True, legend=True, label='Cum_Monthly')
-    ax.legend(loc='upper left', frameon=False, ncol=len(df.columns))
-    ax.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
-    ax2.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
-    plt.savefig('static/chart.png')
-
 
 # Set Matplot parameters
 plt.rcParams['text.color'] = 'black'
--- main.py
+++ main.py
@@ -4,14 +4,55 @@ from wtforms.validators import *
 from flask import Flask, render_template, redirect, url_for, flash, request, send_from_directory
 from flask_bootstrap import Bootstrap
 from flask_wtf.csrf import CSRFProtect
-from functions import *
-from os import remove
+import tempfile
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plt_params
+from matplotlib import ticker
+import tempfile
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ljyhfjfkjfjfou7867856'
 Bootstrap(app)
 csrf = CSRFProtect(app)
+
+# tmp = os.environ.get('tmp')
+
+
+def sigmoid(x, k=3.627412246682911, b=-0.001164921989173435):
+    return 1 / (1 + np.exp(-k*(x-b)))
+
+
+def curve(p_name, contract_amount, start_date, completion_date,  curve, csrf=None):
+    date_range = pd.date_range(start_date, completion_date, freq='d')  # date range per day
+    dur = (date_range.max() - date_range.min()).days
+    if curve == 'trapezoidal':
+        sigmoid_input = np.linspace(-1.1, 1.1, dur + 1) #adjust the x values to better fit the S-Curve function
+        x = sigmoid(sigmoid_input)
+    else:
+        x = np.linspace(0, 1, dur + 1)
+    cum_earnings = (x * contract_amount).ravel()
+    data = pd.DataFrame(cum_earnings, index=date_range, columns=['cum_earnings'])
+    data.iloc[0] = 0
+    # data.iloc[-1] = contract_amount  # last period should be 100% contract amount
+    output = data.resample('M').cum_earnings.max().astype(int).to_frame()
+    output[p_name + '_Monthly'] = output['cum_earnings'].diff().fillna(output['cum_earnings']) #so 1st period won't be NaN
+    output.drop('cum_earnings', inplace=True, axis=1)
+    return output
+
+
+def plot_chart():
+    df = pd.read_csv('/tmp/df.csv', index_col=[0])
+    ax = df.iloc[:, :-2].plot(kind='bar', stacked=True, figsize=(20, 15),
+                              color=['#06283D', '#1363DF', '#47B5FF', '#FF6D02', '#7577CD'])
+    ax2 = df['Cum_Monthly'].plot(c='red', ax=ax, secondary_y=True, legend=True, label='Cum_Monthly')
+    ax.legend(loc='upper left', frameon=False, ncol=len(df.columns))
+    ax.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+    ax2.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+    plt.savefig('/tmp/chart.png')
 
 
 # Create Forms
@@ -63,7 +104,7 @@ def home():
             # combine and process all dataframes in the projects_dfs
             table_df = pd.concat(project_dfs.values(), axis=1).fillna(0).assign(Monthly_Total = lambda x : x.sum(1))
             table_df['Cum_Monthly'] = table_df['Monthly_Total'].cumsum()
-            table_df.to_csv('tmp/df.csv')
+            table_df.to_csv('/tmp/df.csv')
             plot_chart()
 
             # process the combined dataframe to html
@@ -78,7 +119,7 @@ def home():
 
 @app.route('/download/<path:filename>')
 def download(filename):
-    return send_from_directory('static', filename)
+    return send_from_directory('/tmp', filename)
 
 
 
